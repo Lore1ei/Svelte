f52f1dae37ed3c868bab7ee704042219455387f9
Lore1ei <harubator@gmail.com>
2023-06-25 11:41:54 +0700

    fetch params

--- sk-loading-data/src/routes/products/[productId]/+page.server.js
+++ sk-loading-data/src/routes/products/[productId]/+page.server.js
@@ -1,8 +1,14 @@
+import {error} from "@sveltejs/kit";
+
 export const load = async (serverLoadEvent) =>{
     const {fetch, params, url, route} = serverLoadEvent
-    console.log(fetch, params, url, route)
     const {productId} = params
+
     const response = await fetch(`http://localhost:4000/products/${productId}`);
     const product = await response.json();
+    console.log(product)
+    if (Object.keys(product).length === 0) {
+        throw error(404, 'NotFound')
+    }
     return {product}
 }
